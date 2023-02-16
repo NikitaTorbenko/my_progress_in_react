f9ae160dbf46f2282d8e5c4b7c04cd00c3eba845
Nikita Torbenko <2015nekitciti@gmail.com>
2023-02-16 12:15:15 +0300

    htlm + likes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PostsComponent from './Components/PostsComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <PostsComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/LikeComponent.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./LikeComponent.css";
+
+const LikeComponent = () => {
+  const [likeCount, setLikeCount] = React.useState(0);
+  
+  
+
+  const like = () => {
+    console.log("click");
+    setLikeCount((likeCount) => likeCount + 1);
+  };
+
+  const dis = () => {
+    console.log("click");
+    setLikeCount((likeCount) => likeCount - 1);
+  };
+
+  return (
+    <div className="like_block">
+      <span className="like__count">{likeCount}</span>
+      <button className="like__btn" onClick={like}>
+        Like
+      </button>
+      <button className="like__btn" onClick={dis}>
+        Dislike
+      </button>
+    </div>
+  );
+};
+
+export default LikeComponent;
